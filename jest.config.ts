c8fb9f84085847ec2b0f4c8775defc1337ee3fb6
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-20 14:21:48 -0300

    chore: jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
+
+export default config;
