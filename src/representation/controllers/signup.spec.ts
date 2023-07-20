6af5cba6a309881d70f1202deca21127214f6662
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-20 17:20:57 -0300

    test: ensure SignUpController calls EmailInvalidator with correct email

--- src/representation/controllers/signup.spec.ts
+++ src/representation/controllers/signup.spec.ts
@@ -116,4 +116,21 @@ describe("SignUp Controller", () => {
       new InvalidParamError("email")
     );
   });
+
+  test("Should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com');
+  });
 });
