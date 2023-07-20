9e41373aa6829a48f59b92c1922a2fdd493d1064
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-20 19:06:56 -0300

    feat: ensure SignUpController returns 400 if passoword confirmation fails

--- src/representation/controllers/signup.ts
+++ src/representation/controllers/signup.ts
@@ -17,10 +17,15 @@ export class SignUpController implements Controller {
         "password",
         "passwordConfirmation",
       ];
+
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
+      }
+
+      if(httpRequest.body.password !== httpRequest.body.passwordConfirmation){
+        return badRequest(new InvalidParamError('passwordConfirmation'));
       }
 
       const isValid = this.emailValidator.isValid(httpRequest.body.email);
