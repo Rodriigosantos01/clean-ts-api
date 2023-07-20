f51961473d7922cad78050bb11e5a2ce2b574759
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-20 15:29:58 -0300

    test: ensure SignUpController return an error if no name is provide

--- src/representation/controllers/signup.spec.ts
+++ src/representation/controllers/signup.spec.ts
@@ -14,6 +14,7 @@ describe("SignUp Controller", () => {
     };
 
     const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.ststusCode).toBe(400);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
 });
