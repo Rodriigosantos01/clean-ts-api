5d41b81fe8992a6af3d5148e93c68f9de4910c9b
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-20 15:29:49 -0300

    feat: ensure SignUpController return an error if no name is provide

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ import type {Config} from 'jest';
 const config: Config = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
   testEnvironment: 'node',
