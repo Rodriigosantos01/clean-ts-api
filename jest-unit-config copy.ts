a22b01c0624e8af6df2976e79b6d933a3d681c23
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-20 22:09:07 -0300

    chore: add jest configs and scripts

---
+++ jest-unit-config copy.ts
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+  roots: ["<rootDir>/src"],
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: ["**/*.test.ts"]
+};
+
+export default config;
