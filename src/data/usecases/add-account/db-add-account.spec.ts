3d9dfb9bc4ff7cec950631aea3aba566ab1ed3e4
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-21 11:35:24 -0300

    refactor: move encrypter creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -2,37 +2,41 @@ import { Encrypter } from "../../protocols/emcrypter";
 import { DbAddAccount } from "./db-add-account";
 
 interface SutTypes {
-    sut: DbAddAccount
-    encrypterStub: Encrypter
+  sut: DbAddAccount;
+  encrypterStub: Encrypter;
 }
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    }
+  }
+
+  return new EncrypterStub();
+};
 
 const makeSut = (): SutTypes => {
-    class EncrypterStub {
-        async encrypt (value: string): Promise<string> {
-            return new Promise(resolve => resolve('hashed_password'))
-        }
-    }
-    
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbAddAccount(encrypterStub);
-    return {
-        sut,
-        encrypterStub
-    }
-}
+  const encrypterStub = makeEncrypter();
+  const sut = new DbAddAccount(encrypterStub);
+  return {
+    sut,
+    encrypterStub,
+  };
+};
 
-describe('DbAddAccount Usecase', () => {
-    test('Shold call Envrypter with correct password', async () => {
-        const { sut, encrypterStub } = makeSut()
-        const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+describe("DbAddAccount Usecase", () => {
+  test("Shold call Envrypter with correct password", async () => {
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
 
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'valid_password'
-        }
-        
-        await sut.add(accountData)
-        expect(encryptSpy).toHaveBeenCalledWith('valid_password')
-    })
-})
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+
+    await sut.add(accountData);
+    expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+  });
+});
