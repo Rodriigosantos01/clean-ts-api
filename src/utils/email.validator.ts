45abec36eb903710a1d3a052595b788d72c9bf6b
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-21 00:04:16 -0300

    feat: ensure EmailValidatorAdapter returns truevalidator returns true

--- src/utils/email.validator.ts
+++ src/utils/email.validator.ts
@@ -1,8 +1,7 @@
 import { EmailValidator } from "../representation/protocols/email-validator";
-
-
+import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator{
     isValid(email: string): boolean {
-        return false
+        return validator.isEmail(email)
     }
 }
