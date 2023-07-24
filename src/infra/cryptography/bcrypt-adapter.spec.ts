51006866679b6504f6918b7741b0a1d313152533
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-24 16:06:13 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -27,4 +27,15 @@ describe("Bcrypt Adapter", () => {
     const hash = await sut.encrypt("any_value");
     expect(hash).toBe("hash");
   });
+
+  test("Should throw if bcrypt throws", async () => {
+    const sut = makeSut();
+
+    jest.spyOn(bcrypt, "hash").mockImplementationOnce(() => {
+      return new Promise((resolve, reject) => reject(new Error()));
+    });
+
+    const promise = sut.encrypt("any_value");
+    await expect(promise).rejects.toThrow();
+  });
 });
