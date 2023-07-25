e025bb2b9bb36bded097ff3ff70d5497da1db95f
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-25 14:49:37 -0300

    feat: ensure mongodb reconnect isclient is down

--- src/infra/db/mongodb/helpers/mongo-helpers.ts
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -2,14 +2,18 @@ import { Collection, MongoClient } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
+
   async connect(uri: string): Promise<void> {
+    this.uri = uri;
     this.client = await MongoClient.connect(uri);
   },
   async disconnect(): Promise<void> {
     await this.client.close();
+    this.client = null;
   },
 
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
     return this.client.db().collection(name);
   },
 
