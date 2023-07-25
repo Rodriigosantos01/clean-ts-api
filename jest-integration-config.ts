db7e5059e8275279c7af38871d8166692f8851cb
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-25 09:10:29 -0300

    feat: ensure requests parse body as json

---
+++ jest-integration-config.ts
@@ -0,0 +1,22 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+  roots: ["<rootDir>/src"],
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: ["**/*.test.ts"]
+};
+
+export default config;
---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import app from "../config/app";
+
+jest.useRealTimers();
+
+describe("Body Parser Middleware", () => {
+  test("Should parse body as json", async () => {
+    app.post("/test_body_parser", (req, res) => {
+      res.send(req.body);
+    });
+
+    await request(app)
+      .post("/test_body_parser")
+      .send({ name: "Rodrigo" })
+      .expect({ name: "Rodrigo" });
+  });
+});
