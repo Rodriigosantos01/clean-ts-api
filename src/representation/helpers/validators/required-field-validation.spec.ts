4073ddd8458fb98bc7e724c6be83ddfe92753106
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-26 17:08:35 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/representation/helpers/validators/required-field-validation.spec.ts
+++ src/representation/helpers/validators/required-field-validation.spec.ts
@@ -5,7 +5,12 @@ describe("RequiredFiel Validation", () => {
   test("Should return a MissingParamError if validation fails", () => {
     const sut = new RequireFieldValidation("field");
     const error = sut.validate({ name: "any_name " });
-
     expect(error).toEqual(new MissingParamError("field"))
+  });
+
+  test("Should not return if validation succeeds", () => {
+    const sut = new RequireFieldValidation("field");
+    const error = sut.validate({ field: "any_name " });
+    expect(error).toBeFalsy()
   });
 });
