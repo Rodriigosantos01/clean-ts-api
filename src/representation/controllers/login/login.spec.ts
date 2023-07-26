2ac32aa4dda8f8faceb9a6fb9a4cd6cc040a2cb3
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-26 10:56:15 -0300

    refactor: move sut creation to a factory method

--- src/representation/controllers/login/login.spec.ts
+++ src/representation/controllers/login/login.spec.ts
@@ -2,28 +2,38 @@ import { MissingParamError } from "../../errors";
 import { badRequest } from "../../helpers/http-helpers";
 import { LoginController } from "./login";
 
-describe('Login Controller', () => {
-    test('Should return 400 if no email is provided', async() => {
-        const sut = new LoginController()
-        const httpRequest = {
-            body: {
-                password: 'any_password'
-            }
-        }
+interface SutTypes {
+    sut: LoginController
+}
 
-        const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-    });
-    
-    test('Should return 400 if no password is provided', async() => {
-        const sut = new LoginController()
-        const httpRequest = {
-            body: {
-                email: 'any_email@mail.com'
-            }
-        }
+const makeSut = (): SutTypes => {
+  return {
+    sut: new LoginController(),
+  };
+};
 
-        const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
-    });
+describe("Login Controller", () => {
+  test("Should return 400 if no email is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        password: "any_password",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
+  });
+
+  test("Should return 400 if no password is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
 });
