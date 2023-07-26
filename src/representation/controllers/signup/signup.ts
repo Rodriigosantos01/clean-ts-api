f04c2ca3a67d292f109f36075573cfbefe0db46a
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-26 14:01:47 -0300

    refactor: remove anused tests

--- src/representation/controllers/signup/signup.ts
+++ src/representation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, EmailValidator, AddAccount, Validation } from "./signup-protocols";
-import { InvalidParamError, MissingParamError } from "../../errors";
+import { InvalidParamError } from "../../errors";
 import { badRequest, ok, serverError } from "../../helpers/http-helpers";
 
 export class SignUpController implements Controller {
@@ -19,19 +19,7 @@ export class SignUpController implements Controller {
       if (error) {
         return badRequest(error)
       }
-      const requiredFields = [
-        "name",
-        "email",
-        "password",
-        "passwordConfirmation",
-      ];
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
-        }
-      }
-
+      
       const { name, email, password, passwordConfirmation } = httpRequest.body
       if(password !== passwordConfirmation){
         return badRequest(new InvalidParamError('passwordConfirmation'));
