403d2896ecfb3c86e6e1e8da9b547efdcd5cbff8
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-07-31 18:48:44 -0300

    refactor: move sut creatrion to a factory method

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -9,22 +9,27 @@ jest.mock('jsonwebtoken', () => ({
     }
 }))
 
+const makeSut = (): JwtAdapter => {
+    const sut = new JwtAdapter('secret')
+    return sut
+}
+
 describe('Jwt Adapter', () => {
     test('Should call sign with correct values', async () => {
-        const sut = new JwtAdapter('secret')
+        const sut = makeSut()
         const signSpy = jest.spyOn(jwt, 'sign')
         await sut.encrypt('any_id')
         expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
     });
 
     test('Should return a token on sign success', async () => {
-        const sut = new JwtAdapter('secret')
+        const sut = makeSut()
         const accessToken = await sut.encrypt('any_id')
         expect(accessToken).toBe('any_token')
     });
 
     test('Should throw if sign throws', async () => {
-        const sut = new JwtAdapter('secret')
+        const sut = makeSut()
         jest.spyOn(jwt, 'sign').mockImplementationOnce(() => new Promise((resolve, reject) => reject(new Error())))
         const promise = sut.encrypt('any_id')
         await expect(promise).rejects.toThrow()
