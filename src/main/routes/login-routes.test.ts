b0b76fb0fdea3ab94e7b1b9207742b28238dbd66
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-01 11:19:33 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helpers";
+import { Collection } from "mongodb";
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 jest.useRealTimers();
 
@@ -14,7 +18,7 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -27,6 +31,25 @@ describe("Login Routes", () => {
           email: "rodriigosantos01@gmail.com",
           password: "123",
           passwordConfirmation: "123",
+        })
+        .expect(200);
+    });
+  });
+
+  describe("POST /login ", () => {
+    test("Should return 200 on login", async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: "Rodrigo",
+        email: "rodriigosantos01@gmail.com",
+        password
+      })
+
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "rodriigosantos01@gmail.com",
+          password: '123'
         })
         .expect(200);
     });
