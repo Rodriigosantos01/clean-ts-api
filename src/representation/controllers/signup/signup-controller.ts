1d3a1180e57035454e9c5dcf264a48165cf0929a
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-07 11:50:12 -0300

    feat: ensure SignUpController returns an accessToken on success

--- src/representation/controllers/signup/signup-controller.ts
+++ src/representation/controllers/signup/signup-controller.ts
@@ -27,11 +27,11 @@ export class SignUpController implements Controller {
         password,
       });
 
-      await this.authentication.auth({
+      const accessToken = await this.authentication.auth({
         email, password
       })
 
-      return ok(account);
+      return ok({ accessToken });
     } catch (error) {
       return serverError(error);
     }
