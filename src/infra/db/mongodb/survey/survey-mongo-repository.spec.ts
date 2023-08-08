b5a71de9a4e3d2f6193d7f002a511f2a69c9e013
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-08 18:05:24 -0300

    test: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,44 @@
+import { Collection } from "mongodb";
+import { SurveyMongoRepository } from "./survey-mongo-repository";
+import { MongoHelper } from "../helpers/mongo-helpers";
+
+let surveyCollection: Collection
+
+describe('Survey Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        surveyCollection = await MongoHelper.getCollection('surveys')
+        await surveyCollection.deleteMany({})
+    })
+
+    const makeSut = (): SurveyMongoRepository => {
+        return new SurveyMongoRepository()
+    }
+
+    test('Shold add a survey on success', async () => {
+        const sut = makeSut()
+
+        await sut.add({
+            question: 'any_question',
+            answers: [
+                {
+                    image: 'any_image',
+                    answer: 'any_answer'
+                },
+                {
+                    answer: 'other_answer'
+                }
+            ]
+        })
+
+        const survey = await surveyCollection.findOne({ question: 'any_question' })
+        expect(survey).toBeTruthy()
+    })
+})
