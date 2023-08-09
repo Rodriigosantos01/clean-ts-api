693d72ebad9c7ef675c5ca5921ed37190a3632e9
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-09 15:40:41 -0300

    feat: ensure AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/representation/middlewares/auth-middleware.ts
+++ src/representation/middlewares/auth-middleware.ts
@@ -1,25 +1,29 @@
 import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 import { AccessDeniedError } from "../errors";
-import { forbidden, ok } from "../helpers/http/http-helpers";
+import { forbidden, ok, serverError } from "../helpers/http/http-helpers";
 import { HttpRequest, HttpResponse, Middleware } from "../protocols";
 
 export class AuthMiddleware implements Middleware {
     constructor(
         private readonly loadAccountByToken: LoadAccountByToken
-    ) {}
+    ) { }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const accessToken = httpRequest.headers?.['x-access-token']
+        try {
+            const accessToken = httpRequest.headers?.['x-access-token']
 
-        if(accessToken){
-            const account = await this.loadAccountByToken.load(accessToken)
-            if(account){
-                return ok({
-                    accountId: account.id
-                })
+            if (accessToken) {
+                const account = await this.loadAccountByToken.load(accessToken)
+                if (account) {
+                    return ok({
+                        accountId: account.id
+                    })
+                }
             }
-        }
 
-        return forbidden(new AccessDeniedError())
+            return forbidden(new AccessDeniedError())
+        } catch (error) {
+            return serverError(error)
+        }
     }
 
 }
