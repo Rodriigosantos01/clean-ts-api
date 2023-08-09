e1fab031023e8b2641eb7d833b13665635979321
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-09 14:28:25 -0300

    feat: ensure AuthMIddleware returns 403 if no headers are found

---
+++ src/representation/middlewares/auth-middleware.ts
@@ -0,0 +1,11 @@
+import { AccessDeniedError } from "../errors";
+import { forbidden } from "../helpers/http/http-helpers";
+import { HttpRequest, HttpResponse, Middleware } from "../protocols";
+
+export class AuthMiddleware implements Middleware {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const error = forbidden(new AccessDeniedError())
+        return new Promise(resolve => resolve(error))
+    }
+
+}
