7db1241dcaa3c5b3964139745edb82ce20b45707
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-09 15:08:22 -0300

    test: ensure AuthMiddleware return 403 if LoadAccountByToken returns null

--- src/representation/middlewares/auth-middleware.spec.ts
+++ src/representation/middlewares/auth-middleware.spec.ts
@@ -56,4 +56,11 @@ describe('Auth Middleware', () => {
         await sut.handle(makeFakeRequest())
         expect(loadSpy).toHaveBeenCalledWith('any_token')
     });
+
+    test('Should return 403 if LoadAccountByToken returns null', async () => {
+        const { sut, loadAccountByTokenStub } = makeSut()
+        jest.spyOn(loadAccountByTokenStub, 'load').mockRejectedValueOnce(new Promise(resolve => resolve(null)))
+        const HttpResponse = await sut.handle({})
+        expect(HttpResponse).toEqual(forbidden(new AccessDeniedError()))
+    });
 });
