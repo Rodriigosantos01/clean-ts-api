f88ecd2c00c57c3909696cae19bc8e5deee9fc28
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-10 16:33:39 -0300

    test: ensure JwtAdapter thorws if verify throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -51,5 +51,12 @@ describe('Jwt Adapter', () => {
             const value = await sut.decrypt('any_token')
             expect(value).toBe('any_value')
         });
+
+        test('Should throw if verify throws', async () => {
+            const sut = makeSut()
+            jest.spyOn(jwt, 'verify').mockImplementationOnce(() => new Promise((resolve, reject) => reject(new Error())))
+            const promise = sut.decrypt('any_token')
+            await expect(promise).rejects.toThrow()
+        });
     });
 });
