edcbf040308154e92fbf2de16dff77750e3694a3
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-11 11:54:49 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,15 @@
+import { SurveyModel } from "../../../domain/models/survey";
+import { LoadSurveys } from "../../../domain/usecases/load-surveys";
+import { LoadSurveysRepository } from "../../protocols/db/survey/load-survey-repository";
+
+export class DbLoadSurveys implements LoadSurveys {
+    constructor(
+        private readonly loadSurveyRepository: LoadSurveysRepository
+    ){}
+    
+    async load(): Promise<SurveyModel[]> {
+        await this.loadSurveyRepository.loadAll()
+        return []
+    }
+
+}
