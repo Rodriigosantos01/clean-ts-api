6a8efcf73775aa20f6b71520af15ae01fa8848a6
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-11 10:56:06 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

---
+++ src/representation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,12 @@
+import { Controller, HttpRequest, HttpResponse, LoadSurveys } from "./load-survey-controller-protocols";
+
+export class LoadSurveysController implements Controller {
+    constructor(
+        private readonly loadSurveys: LoadSurveys
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        await this.loadSurveys.load()
+        return null
+    }
+}
