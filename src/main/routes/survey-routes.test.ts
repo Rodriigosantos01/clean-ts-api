30a5ef290b2528e0ac51a46cdce0ee86f37d27e7
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-11 12:38:33 -0300

    test: ensure load surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -83,4 +83,49 @@ describe("Survey Routes", () => {
     });
 
   });
+  
+  describe("GET /surveys ", () => {
+    test("Should return 403 on load surveys without accessToken", async () => {
+      await request(app)
+        .get("/api/surveys")
+        .expect(403);
+    });
+
+    // test("Should return 204 on add survey with valid accessToken", async () => {
+    //   const res = await accountCollection.insertOne({
+    //     name: 'Rodrigo',
+    //     email: 'rodrigo@gmail.com',
+    //     password: '123',
+    //     role: 'admin'
+    //   });
+
+    //   const id = res.insertedId;
+    //   const accessToken = sign({ id: `${id}` }, env.jwtSecret)
+
+    //   await accountCollection.updateOne({
+    //     _id: id
+    //   },
+    //     {
+    //       $set: { accessToken }
+    //     })
+
+    //   await request(app)
+    //     .post("/api/surveys")
+    //     .set('x-access-token', accessToken)
+    //     .send({
+    //       question: 'Question',
+    //       answers: [
+    //         {
+    //           answer: 'Answer 1',
+    //           image: 'http://image-name.com'
+    //         },
+    //         {
+    //           answer: 'Answer 2',
+    //         }
+    //       ]
+    //     })
+    //     .expect(204);
+    // });
+
+  });
 });
