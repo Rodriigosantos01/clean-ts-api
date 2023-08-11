af83bd72259e3d3f306f208111c848b96cf30d1d
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-11 16:39:53 -0300

    refactor: git merge conflits

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -2,18 +2,8 @@ import { SurveyAnswerModel } from "../models/survey";
 
 export interface AddSurveyModel {
   question: string;
-<<<<<<< HEAD
-  answers: SurveyAnswer[];
-  date: Date
-}
-
-export interface SurveyAnswer {
-  image?: string;
-  answer: string;
-=======
   answers: SurveyAnswerModel[];
   date: Date
->>>>>>> feat/list-surveys
 }
 
 export interface AddSurvey {
