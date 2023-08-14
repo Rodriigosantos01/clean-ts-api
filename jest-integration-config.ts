0a90eb555048bba64f880c10fa94aeedd85abd7c
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-14 15:05:21 -0300

    chore: change configs jest

--- jest-integration-config.ts
+++ jest-integration-config.ts
@@ -6,17 +6,23 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  roots: ["<rootDir>/src"],
-  clearMocks: true,
-  collectCoverage: false,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.ts$': 'ts-jest'
   },
-  testMatch: ["**/*.test.ts"]
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.test.ts']
 };
 
 export default config;
