b57f79e7eb9730d5c6110128cc125d0bbcd8ca66
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-14 12:25:06 -0300

    feat: ensure DbSaveSurveyResult return a SurveyResult on Success

--- src/data/usecases/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -6,8 +6,8 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
     ) { }
 
     async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-        await this.saveSurveyResultoRepository.save(data)
-        return null
+        const surveyResult = await this.saveSurveyResultoRepository.save(data)
+        return surveyResult
     }
 
 }
