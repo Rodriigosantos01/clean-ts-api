f074338778418dec9815b23e3302cd0c75d9a6d0
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-08-16 20:47:05 -0300

    feat: add swagger api

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URLDbAddAccount || 'mongodb://localhost:27017/clean-node-api',
+    mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
     port: process.env.PORT || 5050,
-    jwtSecret: process.env.JWT_SRCRET || 'tj670==5h'
-}
+    jwtSecret: process.env.JWT_SECRET || 'tj67O==5H'
+  }
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: '3.0.0',
+    info: {
+        title: 'Clena Node Api',
+        description: 'Api do curso do Mango para realizar enquetes entre programadores',
+        version: '1.0.0'
+    }
+}
