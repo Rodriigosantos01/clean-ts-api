4d7d8fee7b1ee25450dc137e8b33d74b9564bf04
Rodrigo Santos <rodriigosantos01@gmail.com>
2023-09-04 12:05:26 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -87,4 +87,26 @@ describe("Survey Routes", () => {
         .expect(403);
     });
   });
+  describe("GET /surveys/:surveyId/results ", () => {
+    test("Should return 200 on load survey result without accessToken", async () => {
+      const accessToken = await makeAccessToken()
+
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+
+      await request(app)
+        .get(`/api/surveys/${res.insertedId}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200);
+    });
+  });
 });
